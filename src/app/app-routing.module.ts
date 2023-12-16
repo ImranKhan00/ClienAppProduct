421d05f8c36199ac7e84e575c8c366a10332dab6
Imran Khan <imrankhandev7@gmail.com>
2023-12-17 01:31:10 +0500

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoryComponent } from './components/category/add-category/add-category.component';
+import { CategoryListComponent } from './components/category/category-list/category-list.component';
+import { EditCategoryComponent } from './components/category/edit-category/edit-category.component';
+import { AddProductComponent } from './components/product/add-product/add-product.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { EditProductComponent } from './components/product/edit-product/edit-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'category/add',
+    component: AddCategoryComponent
+  },
+  {
+    path: 'categories',
+    component: CategoryListComponent
+  },
+  {
+    path: 'category/:id',
+    component: EditCategoryComponent
+  },
+  {
+    path: 'product/add',
+    component: AddProductComponent
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
+  },
+  {
+    path: 'product/:id',
+    component: EditProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './components/category/add-category/add-category.component';
+import { CategoryListComponent } from './components/category/category-list/category-list.component';
+import { EditCategoryComponent } from './components/category/edit-category/edit-category.component';
+import { AddProductComponent } from './components/product/add-product/add-product.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { EditProductComponent } from './components/product/edit-product/edit-product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    CategoryListComponent,
+    EditCategoryComponent,
+    AddProductComponent,
+    ProductListComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
